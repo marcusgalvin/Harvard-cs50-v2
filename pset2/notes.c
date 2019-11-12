914fdff7536a6591f9e9e6193863b1b1ff54f23e
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-11-12 12:51:36 -0500

    notes

---
+++ pset2/notes.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int num = 8;
+  char letter = 68;
+
+  printf("%i\n", num);
+
+  printf("%c\n", letter);
+
+  printf("%c\n", num + 142);
+
+  printf("%i\n", letter + num);
+
+  //     printf("%i\n", m - n);
+
+  printf("%c\n", num + 96);
+}
